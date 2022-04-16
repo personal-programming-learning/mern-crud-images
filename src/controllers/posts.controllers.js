a10cd90b57004eda425800fc1b2e801a00918c4d
Ludwing Rivera Amador <ludwing.ra@gmail.com>
2022-04-15 19:18:11 -0500

    :sparkles: Feature: new functionality for controller CRUD

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -2,17 +2,31 @@
 import PostModel from "../models/Posts.js";
 
 export const getPosts = async(req, res) => {
-  const response = await PostModel.find();
-  res.json(response)
+  try {
+    throw new Error('My new error')
+    const response = await PostModel.find();
+    res.json(response)
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      code: 0,
+      message: error.message
+    })
+  }
 };
 
 export const getByIdPost = async(req, res) => {
   const { id } = req.params;
   try {
     const response = await PostModel.findById(id);
+    if(!response) return res.status(404);
     res.json(response)
   } catch (error) {
-    console.error(error);
+    return res.status(500).json({
+      error: true,
+      code: 0,
+      message: error.message
+    })
   }
 };
 
@@ -24,7 +38,11 @@ export const createPosts = async(req, res) => {
 
     res.json(newPost);
   } catch (error) {
-    console.error(error);
+    return res.status(500).json({
+      error: true,
+      code: 0,
+      message: error.message
+    })
   }
 };
 
@@ -37,7 +55,11 @@ export const updatePosts = async(req, res) => {
     );
     res.json(response)
   } catch (error) {
-    console.error(error);
+    return res.status(500).json({
+      error: true,
+      code: 0,
+      message: error.message
+    })
   }
   
 };
@@ -49,6 +71,11 @@ export const deletePosts = async(req, res) => {
     if(!response) return res.status(404).json({ code: 0, message: 'Not found', error: true })
     res.json({ response, message: 'Post deleting' });
   } catch (error) {
-    console.error(error);
+    console.log(error);
+    return res.status(500).json({
+      error: true,
+      code: 0,
+      message: error.message
+    })
   }
 };
